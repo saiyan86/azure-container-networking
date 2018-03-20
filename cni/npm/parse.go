e3f85be683a4e147f3b47f3a37d4468f7ec804c7
Yongli Chen <yongli.chen@microsoft.com>
2018-03-20 11:21:28 -0700

    add json marshal

--- cni/npm/parse.go
+++ cni/npm/parse.go
@@ -1,6 +1,10 @@
 package npm
 
 import (
+	"encoding/json"
+	"fmt"
+	"os"
+
 	"github.com/davecgh/go-spew/spew"
 
 	networkingv1 "k8s.io/api/networking/v1"
@@ -13,4 +17,9 @@ type policyInfo struct {
 // ParsePolicy parses network policy.
 func ParsePolicy(np *networkingv1.NetworkPolicy) {
 	spew.Dump(np)
+	b, err := json.MarshalIndent(np, "", "  ")
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	os.Stdout.Write(b)
 }
