73a9a2800451dbe8c0ebdaa49715fcd19a576fb5
Yongli Chen <cylcyl86@gmail.com>
2018-03-23 15:46:13 -0700

    fix test 5

--- cni/npm/npm_test.go
+++ cni/npm/npm_test.go
@@ -34,31 +34,31 @@ func TestParseIngress(t *testing.T) {
 	var iptMgr iptm.IptablesManager
 
 	ruleOne := &networkingv1.NetworkPolicyIngressRule{
-		From: []networkingv1.NetworkPolicyPeer{
+		From: []networkingv1.NetworkPolicyPeer{{
 			PodSelector : &metav1.LabelSelector{
 				MatchLabels: map[string]string{"app": "test"},
 			},
-		},
-		Ports: []networkingv1.NetworkPolicyPort{
+		}},
+		Ports: []networkingv1.NetworkPolicyPort{{
 			Protocol: &networkingv1.Protocol{"tcp"},
 			Port: &intstr.IntOrString{
 				StrVal: "8000",
 			},
-		},
+		}},
 	}
 
 	ruleTwo := &networkingv1.NetworkPolicyIngressRule{
-		From: []networkingv1.NetworkPolicyPeer{
+		From: []networkingv1.NetworkPolicyPeer{{
 			PodSelector : &metav1.LabelSelector{
 				MatchLabels: map[string]string{"app": "test"},
 			},
-		},
-		Ports: []networkingv1.NetworkPolicyPort{
+		}},
+		Ports: []networkingv1.NetworkPolicyPort{{
 			Protocol: &networkingv1.Protocol{"udp"},
 			Port: &intstr.IntOrString{
 				StrVal: "8001",
 			},
-		},
+		}},
 	}	
 
 	var rules []networkingv1.NetworkPolicyIngressRule
