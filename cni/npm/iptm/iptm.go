a293f6371709f5578402d3a44eb30906095db2d0
Yongli Chen <yongli.chen@microsoft.com>
2018-05-14 13:57:51 -0700

    making delete idempotent

--- cni/npm/iptm/iptm.go
+++ cni/npm/iptm/iptm.go
@@ -307,11 +307,20 @@ func (iptMgr *IptablesManager) Add(entry *IptEntry) error {
 
 // Delete removes an entry from entryMap, and deletes the corresponding iptables rule.
 func (iptMgr *IptablesManager) Delete(entry *IptEntry) error {
+	fmt.Printf("%+v\n", entry)
+
+	exists, err := iptMgr.Exists(entry)
+	if err != nil {
+		return err
+	}
+
+	if !exists {
+		return nil
+	}
 	// Create iptables rules for every entry in the entryMap.
 	iptMgr.OperationFlag = util.IptablesDeletionFlag
-	fmt.Printf("%+v\n", entry)
 	if _, err := iptMgr.Run(entry); err != nil {
-		fmt.Printf("Error creating iptables rules.\n")
+		fmt.Printf("Error deleting iptables rules.\n")
 		return err
 	}
 
