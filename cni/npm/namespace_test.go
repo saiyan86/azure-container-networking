ba75a271a7aec9b0f1338a1d0ff69e84cd6318de
Yongli Chen <yongli.chen@microsoft.com>
2018-05-17 11:34:03 -0700

    add namespace test

---
+++ cni/npm/namespace_test.go
@@ -0,0 +1,28 @@
+package npm
+
+import (
+	"testing"
+)
+
+func TestnewNs(t *testing.T) {
+	if _, err := newNs("test"); err != nil {
+		t.Errorf("TestnewNs failed @ newNs")
+	}
+}
+
+func TestAllNsList(t *testing.T) {
+	ns, err := newNs("test")
+	if err != nil {
+		t.Errorf("TestAllNsList failed @ newNs")
+	}
+
+	npMgr := &NetworkPolicyManager{}
+
+	if err := npMgr.InitAllNsList(ns); err != nil {
+		t.Errorf("TestAllNsList failed @ InitAllNsList")
+	}
+
+	if err := npMgr.UninitAllNsList(ns); err != nil {
+		t.Errorf("TestAllNsList failed @ UninitAllNsList")
+	}
+}
