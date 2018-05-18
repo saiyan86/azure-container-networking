bd29230df2911f22c787ce140b65a70cc5f82d0a
Yongli Chen <yongli.chen@microsoft.com>
2018-05-17 17:32:37 -0700

    TestRestore

--- cni/npm/iptm/iptm_test.go
+++ cni/npm/iptm/iptm_test.go
@@ -2,9 +2,18 @@ package iptm
 
 import "testing"
 
+/*
 func TestSave(t *testing.T) {
 	iptMgr := &IptablesManager{}
 	if err := iptMgr.Save(); err != nil {
 		t.Errorf("TestSave failed @ iptMgr.Save()")
+	}
+}
+*/
+
+func TestRestore(t *testing.T) {
+	iptMgr := &IptablesManager{}
+	if err := iptMgr.Restore(); err != nil {
+		t.Errorf("TestRestore failed @ iptMgr.Restore()")
 	}
 }
