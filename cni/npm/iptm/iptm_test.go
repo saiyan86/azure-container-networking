e4a6c02c29ab03441b89f402d0f571ee40e3bfc3
Yongli Chen <yongli.chen@microsoft.com>
2018-05-18 10:54:50 -0700

    TestAddNetworkPolicy

--- cni/npm/iptm/iptm_test.go
+++ cni/npm/iptm/iptm_test.go
@@ -5,13 +5,13 @@ import "testing"
 func TestSave(t *testing.T) {
 	iptMgr := &IptablesManager{}
 	if err := iptMgr.Save(); err != nil {
-		t.Errorf("TestSave failed @ iptMgr.Save()")
+		t.Errorf("TestSave failed @ iptMgr.Save")
 	}
 }
 
 func TestRestore(t *testing.T) {
 	iptMgr := &IptablesManager{}
 	if err := iptMgr.Restore(); err != nil {
-		t.Errorf("TestRestore failed @ iptMgr.Restore()")
+		t.Errorf("TestRestore failed @ iptMgr.Restore")
 	}
 }
--- cni/npm/nwpolicy_test.go
+++ cni/npm/nwpolicy_test.go
@@ -3,8 +3,12 @@ package npm
 import (
 	"testing"
 
+	"github.com/Azure/azure-container-networking/cni/npm/iptm"
+
+	corev1 "k8s.io/api/core/v1"
 	networkingv1 "k8s.io/api/networking/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
 )
 
 func TestAddNetworkPolicy(t *testing.T) {
@@ -12,10 +16,41 @@ func TestAddNetworkPolicy(t *testing.T) {
 		nsMap: make(map[string]*namespace),
 	}
 
+	tcp := corev1.ProtocolTCP
 	allow := &networkingv1.NetworkPolicy{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      "allow-ingress",
 			Namespace: "test-nwpolicy",
 		},
+		Spec: networkingv1.NetworkPolicySpec{
+			Ingress: []networkingv1.NetworkPolicyIngressRule{
+				networkingv1.NetworkPolicyIngressRule{
+					From: []networkingv1.NetworkPolicyPeer{{
+						PodSelector: &metav1.LabelSelector{
+							MatchLabels: map[string]string{"app": "test"},
+						},
+					}},
+					Ports: []networkingv1.NetworkPolicyPort{{
+						Protocol: &tcp,
+						Port: &intstr.IntOrString{
+							StrVal: "8000",
+						},
+					}},
+				},
+			},
+		},
+	}
+
+	iptMgr := iptm.IptablesManager{}
+	if err := iptMgr.Save(); err != nil {
+		t.Errorf("TestAddNetworkPolicy failed @ iptMgr.Save")
+	}
+
+	if err := npMgr.AddNetworkPolicy(allow); err != nil {
+		t.Errorf("TestAddNetworkPolicy failed @ AddNetworkPolicy")
+	}
+
+	if err := iptMgr.Restore(); err != nil {
+		t.Errorf("TestAddNetworkPolicy failed @ iptMgr.Restore")
 	}
 }
