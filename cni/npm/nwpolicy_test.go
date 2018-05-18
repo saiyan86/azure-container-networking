ceb88bc2df78467930ea90a5b45d7ffaf4231565
Yongli Chen <yongli.chen@microsoft.com>
2018-05-18 15:54:15 -0700

    TestUpdateNetworkPolicy

--- cni/npm/nwpolicy_test.go
+++ cni/npm/nwpolicy_test.go
@@ -75,3 +75,94 @@ func TestAddNetworkPolicy(t *testing.T) {
 	}
 
 }
+
+func TestUpdateNetworkPolicy(t *testing.T) {
+	npMgr := &NetworkPolicyManager{
+		nsMap: make(map[string]*namespace),
+	}
+
+	nsObj := &corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "test-nwpolicy",
+			Labels: map[string]string{
+				"app": "test-namespace",
+			},
+		},
+	}
+
+	if err := npMgr.AddNamespace(nsObj); err != nil {
+		fmt.Errorf("TestAddNetworkPolicy @ npMgr.AddNamespace")
+	}
+
+	tcp, udp := corev1.ProtocolTCP, corev1.ProtocolUDP
+	allowIngress := &networkingv1.NetworkPolicy{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "allow-ingress",
+			Namespace: "test-nwpolicy",
+		},
+		Spec: networkingv1.NetworkPolicySpec{
+			Ingress: []networkingv1.NetworkPolicyIngressRule{
+				networkingv1.NetworkPolicyIngressRule{
+					From: []networkingv1.NetworkPolicyPeer{{
+						PodSelector: &metav1.LabelSelector{
+							MatchLabels: map[string]string{"app": "test"},
+						},
+					}},
+					Ports: []networkingv1.NetworkPolicyPort{{
+						Protocol: &tcp,
+						Port: &intstr.IntOrString{
+							StrVal: "8000",
+						},
+					}},
+				},
+			},
+		},
+	}
+
+	allowEgress := &networkingv1.NetworkPolicy{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "allow-egress",
+			Namespace: "test-nwpolicy",
+		},
+		Spec: networkingv1.NetworkPolicySpec{
+			Egress: []networkingv1.NetworkPolicyEgressRule{
+				networkingv1.NetworkPolicyEgressRule{
+					To: []networkingv1.NetworkPolicyPeer{{
+						NamespaceSelector: &metav1.LabelSelector{
+							MatchLabels: map[string]string{"ns": "test"},
+						},
+					}},
+					Ports: []networkingv1.NetworkPolicyPort{{
+						Protocol: &udp,
+						Port: &intstr.IntOrString{
+							StrVal: "8001",
+						},
+					}},
+				},
+			},
+		},
+	}
+
+	iptMgr := iptm.IptablesManager{}
+	if err := iptMgr.Save(); err != nil {
+		t.Errorf("TestUpdateNetworkPolicy failed @ iptMgr.Save")
+	}
+
+	if err := npMgr.AddNetworkPolicy(allowIngress); err != nil {
+		t.Errorf("TestUpdateNetworkPolicy failed @ AddNetworkPolicy")
+	}
+
+	if err := npMgr.UpdateNetworkPolicy(allowIngress, allowEgress); err != nil {
+		t.Errorf("TestUpdateNetworkPolicy failed @ UpdateNetworkPolicy")
+	}
+
+	if err := iptMgr.Restore(); err != nil {
+		t.Errorf("TestUpdateNetworkPolicy failed @ iptMgr.Restore")
+	}
+
+	ipsMgr := &ipsm.IpsetManager{}
+	if err := ipsMgr.Destroy(); err != nil {
+		t.Errorf("TestUpdateNetworkPolicy failed @ ns.ipsMgr.Destroy")
+	}
+
+}
