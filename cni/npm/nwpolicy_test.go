6b916c6437f632387152c1376b34df1e26331143
Yongli Chen <yongli.chen@microsoft.com>
2018-05-18 16:25:06 -0700

    TestDeleteNetworkPolicy

--- cni/npm/nwpolicy_test.go
+++ cni/npm/nwpolicy_test.go
@@ -166,3 +166,70 @@ func TestUpdateNetworkPolicy(t *testing.T) {
 	}
 
 }
+
+func TestDeleteNetworkPolicy(t *testing.T) {
+	npMgr := &NetworkPolicyManager{
+		nsMap: make(map[string]*namespace),
+	}
+
+	nsObj := &corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "test-nwpolicy",
+			Labels: map[string]string{
+				"app": "test-namespace",
+			},
+		},
+	}
+
+	if err := npMgr.AddNamespace(nsObj); err != nil {
+		fmt.Errorf("TestAddNetworkPolicy @ npMgr.AddNamespace")
+	}
+
+	tcp := corev1.ProtocolTCP
+	allow := &networkingv1.NetworkPolicy{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "allow-ingress",
+			Namespace: "test-nwpolicy",
+		},
+		Spec: networkingv1.NetworkPolicySpec{
+			Ingress: []networkingv1.NetworkPolicyIngressRule{
+				networkingv1.NetworkPolicyIngressRule{
+					From: []networkingv1.NetworkPolicyPeer{{
+						PodSelector: &metav1.LabelSelector{
+							MatchLabels: map[string]string{"app": "test"},
+						},
+					}},
+					Ports: []networkingv1.NetworkPolicyPort{{
+						Protocol: &tcp,
+						Port: &intstr.IntOrString{
+							StrVal: "8000",
+						},
+					}},
+				},
+			},
+		},
+	}
+
+	iptMgr := iptm.IptablesManager{}
+	if err := iptMgr.Save(); err != nil {
+		t.Errorf("TestAddNetworkPolicy failed @ iptMgr.Save")
+	}
+
+	if err := npMgr.AddNetworkPolicy(allow); err != nil {
+		t.Errorf("TestAddNetworkPolicy failed @ AddNetworkPolicy")
+	}
+
+	if err := npMgr.DeleteNetworkPolicy(allow); err != nil {
+		t.Errorf("TestAddNetworkPolicy failed @ DeleteNetworkPolicy")
+	}
+
+	if err := iptMgr.Restore(); err != nil {
+		t.Errorf("TestAddNetworkPolicy failed @ iptMgr.Restore")
+	}
+
+	ipsMgr := &ipsm.IpsetManager{}
+	if err := ipsMgr.Destroy(); err != nil {
+		t.Errorf("TestAddNamespace failed @ ns.ipsMgr.Destroy")
+	}
+
+}
