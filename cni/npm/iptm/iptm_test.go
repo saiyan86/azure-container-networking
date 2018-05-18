83af31f75fe1b1a2c72493fda78b47d92e3ccc97
Yongli Chen <yongli.chen@microsoft.com>
2018-05-17 17:35:51 -0700

    uncomment TestSave

--- cni/npm/iptm/iptm_test.go
+++ cni/npm/iptm/iptm_test.go
@@ -2,14 +2,12 @@ package iptm
 
 import "testing"
 
-/*
 func TestSave(t *testing.T) {
 	iptMgr := &IptablesManager{}
 	if err := iptMgr.Save(); err != nil {
 		t.Errorf("TestSave failed @ iptMgr.Save()")
 	}
 }
-*/
 
 func TestRestore(t *testing.T) {
 	iptMgr := &IptablesManager{}
