49047b99677eead47efd642d15183470a602efd5
Yongli Chen <yongli.chen@microsoft.com>
2019-03-13 16:44:50 -0700

    update npm script

--- scripts/install-npm-dependencies.sh
+++ scripts/install-npm-dependencies.sh
@@ -4,10 +4,10 @@ IPTABLES_ZIP_URL=http://www.netfilter.org/projects/iptables/files/
 IPTABLES_ZIP_FILE_NAME=iptables-1.8.2
 
 apt-get update
-apt-get install -y ipset
-apt-get install -y wget
-apt-get install -y make
-apt-get install -y gcc
+apt-get install -y ipset \
+wget \
+make \
+gcc
 
 wget ${IPTABLES_ZIP_URL}${IPTABLES_ZIP_FILE_NAME}.tar.bz2
 tar xjf ${IPTABLES_ZIP_FILE_NAME}.tar.bz2 && cd ${IPTABLES_ZIP_FILE_NAME}
@@ -25,8 +25,8 @@ do
   ln -sfv ../../lib/$(readlink /usr/lib/lib${file}.so) /usr/lib/lib${file}.so
 done
 
-rm -rf ../${IPTABLES_ZIP_FILE_NAME}*
+apt-get purge -y wget \
+make \
+gcc
 
-apt-get purge -y wget
-apt-get purge -y make
-apt-get purge -y gcc
+rm -rf ../${IPTABLES_ZIP_FILE_NAME}*
